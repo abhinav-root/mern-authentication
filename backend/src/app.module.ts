b812e68e07c8abc3ebab89bae2126bec74189c08
Abhinav <abhinavsinghkkr@gmail.com>
2024-09-14 10:07:09 +0530

    Connect mongodb to nestjs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'development.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
